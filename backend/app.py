f1e88ec3f8b081c5fcc1c0c8c536869520d77ce9
Chirag Goswami <8825209057@mail.jaypeeu.ac.in>
2026-09-02 13:19:43 -0700

    fixed neon ssl

--- backend/app.py
+++ backend/app.py
@@ -16,6 +16,11 @@ from routes.meta import meta_bp
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+        'pool_pre_ping': True,
+        'pool_recycle': 300,
+}
     # Cloudinary Setup
     cloudinary.config(
         cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
